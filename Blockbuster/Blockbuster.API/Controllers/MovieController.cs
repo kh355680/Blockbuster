7eb8668651060903e225db159408af17c3ee0b3f
KH Arif Ahmed <KH Arif Ahmed>
2016-07-23 01:44:20 +0600

    no message

--- Blockbuster/Blockbuster.API/Controllers/MovieController.cs
+++ Blockbuster/Blockbuster.API/Controllers/MovieController.cs
@@ -1,4 +1,8 @@
-﻿using Blockbuster.BusinessModel;
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Blockbuster.BusinessModel;
 using Blockbuster.BusinessModel.Entities;
 using Blockbuster.Repositories.Repositories;
 using Blockbuster.Service.Service;
@@ -10,6 +14,25 @@ namespace Blockbuster.API.Controllers
         public MovieController():base(new MovieService(new MovieRepository(new Context())))
         {
             
+        }
+
+        [Route("api/movie/casting/{movieId}")]
+        [HttpGet]
+        public IHttpActionResult MovieCasting([FromUri]string movieId)
+        {
+            var db = new Context();
+            var casting = db.MovieCasts.Where(m => m.MovieId == movieId).ToList();
+
+            List<Artist> actors = new List<Artist>();
+            
+            foreach (var movieCast in casting)
+            {
+                var actor = db.Artists.Find(movieCast.ArtistId);
+                actors.Add(actor);
+            }
+
+            return Ok(actors);
+                       
         }
     }
 }
